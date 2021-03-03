5164b6eef55481c77c4342a0247d5b18afe82f06
Anna Filipiak <anna.filipiak@collibra.com>
2021-03-03 11:03:42 +0100

    Pack into class

--- src/string_utils.py
+++ src/string_utils.py
@@ -2,44 +2,51 @@ import random
 from string import ascii_lowercase,punctuation,digits
 
 
-_chars = ascii_lowercase+punctuation+digits
+class StringUtils(object):
 
-def random_char():
-    return random.choice(_chars)
+    _chars = ascii_lowercase+punctuation+digits
 
-
-def random_alpha():
-    return random.choice(ascii_lowercase)
-
-
-def random_delete(word_as_array):
-    place = random.randint(0, len(word_as_array) - 1)
-    del word_as_array[place]
-    return word_as_array
+    @staticmethod
+    def random_char():
+        return random.choice(StringUtils._chars)
 
 
-def random_add(word_as_array):
-    place = random.randint(0, len(word_as_array))
-    word_as_array.insert(place, random_alpha())
-    return word_as_array
+    @staticmethod
+    def random_alpha():
+        return random.choice(ascii_lowercase)
 
 
-def random_replace(word_as_array):
-    if not len(word_as_array):
+    @staticmethod
+    def random_delete(word_as_array):
+        place = random.randint(0, len(word_as_array) - 1)
+        del word_as_array[place]
         return word_as_array
-    place = random.randint(0, len(word_as_array) - 1) if len(word_as_array) > 1 else 0
-    word_as_array[place] = random_alpha()
-    return word_as_array
 
 
-_noise_fun = [random_delete, random_add, random_replace]
-
-# assume all are lowercase
-def add_noise(word, n_changes=1):
-    noisy = [c for c in word]
-    for i in range(n_changes):
-        start = 0 if len(noisy) > 1 else 1
-        noisy = random.choice(_noise_fun[start:])(noisy)
-    return "".join(noisy)
+    @staticmethod
+    def random_add(word_as_array):
+        place = random.randint(0, len(word_as_array))
+        word_as_array.insert(place, StringUtils.random_alpha())
+        return word_as_array
 
 
+    @staticmethod
+    def random_replace(word_as_array):
+        if not len(word_as_array):
+            return word_as_array
+        place = random.randint(0, len(word_as_array) - 1) if len(word_as_array) > 1 else 0
+        word_as_array[place] = StringUtils.random_alpha()
+        return word_as_array
+
+
+    # assume all are lowercase
+    @staticmethod
+    def add_noise(word, n_changes=1):
+        noisy = [c for c in word]
+        for i in range(n_changes):
+            start = 0 if len(noisy) > 1 else 1
+            noisy = random.choice(_noise_fun[start:])(noisy)
+        return "".join(noisy)
+
+
+_noise_fun = [StringUtils.random_delete, StringUtils.random_add, StringUtils.random_replace]
